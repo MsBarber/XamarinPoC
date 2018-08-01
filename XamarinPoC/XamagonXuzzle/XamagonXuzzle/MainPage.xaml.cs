9c8508b4d52c3fcf6fce93dc2289ef0df99d0607
Neil <revon210@hotmail.com>
2018-08-01 14:09:18 -0400

    Init

---
+++ XamarinPoC/XamagonXuzzle/XamagonXuzzle/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using NBitcoin;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamagonXuzzle
+{
+    public partial class MainPage : ContentPage
+    {
+
+        Key privKey = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        void GenerateNewMnemonic(object sender, EventArgs args)
+        {
+
+            Mnemonic mnemo = new Mnemonic(Wordlist.English);
+            ExtKey hdRoot = mnemo.DeriveExtKey("my password");
+            message.Text = mnemo.ToString();
+        }
+
+        void GeneratePrivateKey(object sender, EventArgs args)
+        {
+            privKey = new Key();
+
+            priv.Text = privKey.GetBitcoinSecret(Network.Main).ToString();
+        }
+
+
+        void GenerateAddress(object sender, EventArgs args)
+        {
+            if (privKey == null)
+            {
+                addy.Text = "Please generate a private key first.";
+                return;
+            }
+
+            addy.Text = privKey.PubKey.GetAddress(Network.Main).ToString();
+        }
+       
+    }
+}
